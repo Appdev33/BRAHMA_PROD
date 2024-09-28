54e98ffc894433d5821c93371a4666dff686e9a1
appdev <app.devplatform@gmail.com>
2024-09-28 16:12:44 +0530

    initial commit ride app skeleton

---
+++ uber_microservice/ride-service/src/test/java/com/brahma/ride_service/RideServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brahma.ride_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
